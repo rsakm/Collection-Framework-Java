d5dbea62c6520b83bcaded0560765e95084806c7
Rajshree <rajshreeakm@gmail.com>
2025-04-25 21:01:19 +0530

    Collection Object

---
+++ src/collection/Demo.java
@@ -0,0 +1,17 @@
+package collection;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class Demo {
+    public static void main(String[] args) {
+        ArrayList al = new ArrayList();
+        al.add(10);
+        al.add("rajshree");
+        al.add('r');    ////collection object
+
+        HashSet hs = new HashSet();
+        hs.add("Rajshree");
+        hs.add(20);
+    }
+}
